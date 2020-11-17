64309acbc2368508966cde72e1bd1147905bbecf
chrysalis <b4baker@gmail.com>
2020-11-17 13:40:04 +0900

    link resource files

--- html/public/index.php
+++ html/public/index.php
@@ -16,8 +16,8 @@ $twig = Twig::create(__DIR__ . '/../src/views',
 // Add Twig-View Middleware
 $app->add(TwigMiddleware::create($app, $twig));
 
-// routes
-$app->get('/', function ($request, $response, $args) {
+// view route
+$app->get('/recruitment', function ($request, $response, $args) {
     //get db settings
     $dbSetting = Settings\settings::getDbSetting();
     $view = Twig::fromRequest($request);
@@ -30,6 +30,40 @@ $app->get('/', function ($request, $response, $args) {
     }
 
     return $view->render($response, 'top.html', ["res" => $res]);
+});
+
+// view resource files
+$app->get('/css/{file}', function ($request, $response, $args) {
+    $file = "css/" . $args['file'];
+    if ($data = file_get_contents($file)) {
+        $response->getBody()->write($data);
+        return $response->withHeader('Content-Type', 'text/css');
+    } else {
+        return $response->withStatus(404);
+    }
+});
+
+$app->get('/js/{file}', function ($request, $response, $args) {
+    $file = "js/" . $args['file'];
+    if ($data = file_get_contents($file)) {
+        $response->getBody()->write($data);
+        return $response->withHeader('Content-Type', 'text/javascript');
+    } else {
+        return $response->withStatus(404);
+    }
+});
+
+$app->get('/img/{file}', function ($request, $response, $args) {
+    $file = "img/" . $args['file'];
+    if ($data = file_get_contents($file)) {
+        $finfo = finfo_open(FILEINFO_MIME_TYPE);
+        $contenttype = finfo_file($finfo, $file);
+        finfo_close($finfo);
+        $response->getBody()->write($data);
+        return $response->withHeader('Content-Type', $contenttype);
+    } else {
+        return $response->withStatus(404);
+    }
 });
 
 // Run the app
