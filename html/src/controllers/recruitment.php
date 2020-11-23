e91b14a4e143e1bd07a3b22f435aa34815f632f9
chrysalis <b4baker@gmail.com>
2020-11-23 16:12:09 +0900

    operator API mod

--- html/src/controllers/recruitment.php
+++ html/src/controllers/recruitment.php
@@ -76,10 +76,18 @@ class recruitment {
         }
 
         // query db to get the type of each label
-        // xLabels: ["label_i" => label]
+        // xParams: ["label_i" => param]
+        // used in bind-param
+        $xParams = [];
+
+        // xAffices: [:label_i]
+        // used in query statement
+        $xAffices = [];
+
+        // xLabels: ["o.type=:label_i"]
+        // used in query statement
         $xLabels = [];
-        // xStmt: ["type=:label_i"]
-        $xStmt = [];
+
         // and this is i
         $index = 0;
         $sql = "select type from recruitment_label where value=:value";
@@ -91,32 +99,80 @@ class recruitment {
             $stmt = $this->dbConn->prepare($sql);
             $stmt->execute([':value' => $label]);
             $type = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
             // skip if type is empty
             if (empty($type)) {
                 continue;
             }
-            // if affix, use INSTR
+
+            // if affix, append to xAffices
             if ($type[0]['type'] == "affix") {
-                $xLabels[':affix_' . $index] = $label;
-                $xStmt[] = 'instr(affix,:affix_' . $index . ')';
-            } else {
-                $xLabels[':label_' . $index] = $label;
-                $xStmt[] = $type[0]['type'] . '=:label_' . $index;
+                $xAffices[] = ":label_" . $index;
+            } else { // otherwise append to xLabels
+                $xLabels[] = "o." . $type[0]['type'] . "=:label_" . $index;
             }
-                $index++;
+            $xParams[':label_' . $index] = $label;
+            $index++;
+        }
+
+        // return empty array if no effective label
+        if (empty($xAffices) && empty($xLabels)) {
+            return ["operators" => []];
+        }
+
+        // return all operators if "all" is in labels
+        if (in_array("all", $labels, true)) {
+            $sql = 'select o.*,group_concat(a.affix separator ",") as affix from recruitment_operator as o join operator_affix as a where a.codename=o.codename group by o.codename';
+            $stmt = $this->dbConn->prepare($sql);
+            $stmt->execute();
+            $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            $this->dbConn = null;
+            return ["operators" => $result];
+        }
+
+        // query codenames from xAffices and xLabels
+        // statement eg:
+        // ------------------- //
+        // SELECT o.codename
+        // FROM
+        //   recruitment_operator AS o JOIN operator_affix AS a
+        // WHERE
+        //   (o.codename=a.codename AND a.affix IN (affix...))
+        // OR
+        //   (o.codename=a.codename AND (o.class=... OR o.position=...))
+        // GROUP BY o.codename
+        // ------------------- //
+        $sql = "select o.codename from recruitment_operator as o join operator_affix as a where ";
+        if (!empty($xAffices)) {
+            $subSql1 = "(o.codename=a.codename and a.affix in (" . implode(",", $xAffices) . "))";
+        } else {
+            $subSql1 = "false";
+        }
+        if (!empty($xLabels)) {
+            $subSql2 = "(o.codename=a.codename AND (" . implode(" or ", $xLabels) . "))";
+        } else {
+            $subSql2 = "false";
+        }
+        $sql .= $subSql1 . " or " . $subSql2 . " group by o.codename";
+        $stmt = $this->dbConn->prepare($sql);
+        $stmt->execute($xParams);
+        $codenames = $stmt->fetchAll(PDO::FETCH_COLUMN, 0);
+
+        // generate statement and params of codenames
+        $index = 0;
+        $xCodes = [];
+        $xParams = [];
+        foreach ($codenames as $codename) {
+            $xCodes[] = ":code_" . $index;
+            $xParams[":code_" . $index] = $codename;
+            $index++;
         }
 
         // query operators
         // if "all" is included, query all operators
-        if (in_array("all", $labels, true)) {
-            $sql = "select * from recruitment_operator";
-        } elseif (!empty($xStmt)) {
-            $sql = "select * from recruitment_operator where " . implode(" or ", $xStmt);
-        } else {
-            return ["operators" => []];
-        }
+        $sql = 'select o.*,group_concat(a.affix separator ",") as affix from recruitment_operator as o join operator_affix as a where a.codename=o.codename and o.codename in (' . implode(",", $xCodes) . ") group by o.codename";
         $stmt = $this->dbConn->prepare($sql);
-        $stmt->execute($xLabels);
+        $stmt->execute($xParams);
         $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
         $this->dbConn = null;
